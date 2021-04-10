b9ab1b69f49429b66a350ac3506c7221cafebc15
Rohan Vaghani <rohanvaghani@gmail.com>
2021-04-10 15:22:37 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="container">
+  <Header title="Task Tracker" />
+  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+</div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id)
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task )
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Clinic Appointment',
+        day: 'March 3rd at 8:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'College Appointment',
+        day: 'March 11th at 1:30am',
+        reminder: false,
+      }
+    ]
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,35 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>
+            {{ task.text }}
+            <i @click="$emit('delete-task',task.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{ task.day }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object,
+    },
+}
+</script>
+
+<style scoped>
+.fas {
+    color: red;
+}
+
+.task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+}
+
+.reminder {
+    border-left: 3px green solid;
+}
+
+</style>
